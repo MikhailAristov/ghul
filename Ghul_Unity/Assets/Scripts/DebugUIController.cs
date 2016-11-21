54bc4b2816eeeef5a245c19484ba1bfdc104c888
JakobDahlum <ugdrt@student.kit.edu>
2016-11-21 23:48:05 +0100

    Debug Fenster eingebaut, Ausdauer für Rennen implementiert

---
+++ Ghul_Unity/Assets/Scripts/DebugUIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DebugUIController : MonoBehaviour {
+
+	public Text displayedText;
+	public GameObject player;
+	private PlayerController playerController;
+
+	// Use this for initialization
+	void Start () {
+		playerController = player.GetComponent<PlayerController> ();
+		displayedText.text = "Debug Window";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		displayedText.text = "DebugWindow\n"
+			+ "Speed: " + playerController.GetSpeed() + "\n"
+			+ "Stamina: " + playerController.GetStamina() + "\n"
+			+ "isOutOfPower: " + playerController.IsOutOfPower() + "\n";
+	}
+}
--- Ghul_Unity/Assets/Scripts/PlayerController.cs
+++ Ghul_Unity/Assets/Scripts/PlayerController.cs
@@ -14,11 +14,19 @@ public class PlayerController : MonoBehaviour {
 	private const float MAX_RUN_SPEED_MULTIPLIER = 2.0f;
 	private const float BASIC_MOVEMENT_SPEED = 5.0f;
 
+	private float stamina;
+	private const float MAX_STAMINA = 10.0f;
+	private const float STAMINA_RECOVERY_SPEED = 2.0f;
+	private bool isOutOfPower; // if all stamina is used up, player can't run until stamina is full again TODO: Stamina bar (?)
+
 	private Transform trafo; // contains position, rotation, scale of the player
 
 	// Use this for initialization
 	void Start () {
 		runSpeedMultiplier = 1.0f;
+		stamina = MAX_STAMINA;
+		isOutOfPower = false;
+
 		trafo = transform;
 		blockLeftTrafo = movementBlockerLeft.transform;
 		blockRightTrafo = movementBlockerRight.transform;
@@ -28,14 +36,24 @@ public class PlayerController : MonoBehaviour {
 	public float GetSpeed() {
 		return BASIC_MOVEMENT_SPEED * runSpeedMultiplier;
 	}
+
+	// returns the current stamina value
+	public float GetStamina() {
+		return stamina;
+	}
+
+	public bool IsOutOfPower() {
+		return isOutOfPower;
+	}
 	
 	// Update is called once per frame
+	//TODO: Move content to sub functions to avoid cluttering
 	void Update () {
-		print (BASIC_MOVEMENT_SPEED * runSpeedMultiplier);
+		//print (BASIC_MOVEMENT_SPEED * runSpeedMultiplier); //prints the speed to the console
 		bool directionKeyPressed = (Input.GetAxis ("Horizontal") > 0.01f || Input.GetAxis ("Horizontal") < -0.01f);
 
 		// Run speed handling
-		if (Input.GetButton ("Run") && directionKeyPressed) {
+		if (Input.GetButton ("Run") && directionKeyPressed && !isOutOfPower) {
 			runSpeedMultiplier = Mathf.Min (MAX_RUN_SPEED_MULTIPLIER, runSpeedMultiplier + RUN_SPEED_MULTIPLIER_INCREASEMENT * Time.deltaTime);
 		} else {
 			runSpeedMultiplier = Mathf.Max (1.0f, runSpeedMultiplier - RUN_SPEED_MULTIPLIER_DECREASEMENT * Time.deltaTime);
@@ -52,6 +70,19 @@ public class PlayerController : MonoBehaviour {
 			trafo.position = new Vector3(blockLeftTrafo.position.x, trafo.position.y, trafo.position.z);
 		} else if (trafo.position.x >= blockRightTrafo.position.x) {
 			trafo.position = new Vector3(blockRightTrafo.position.x, trafo.position.y, trafo.position.z);
+		}
+
+		// Stamina handling
+		if (runSpeedMultiplier > 1.0f) {
+			stamina = Mathf.Max (0.0f, stamina - runSpeedMultiplier * Time.deltaTime);
+			if (stamina == 0.0f) {
+				isOutOfPower = true;
+			}
+		} else {
+			stamina = Mathf.Min(MAX_STAMINA, stamina + STAMINA_RECOVERY_SPEED * Time.deltaTime);
+			if (stamina == MAX_STAMINA) {
+				isOutOfPower = false;
+			}
 		}
 	}
 }
