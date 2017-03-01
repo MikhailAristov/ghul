83755b0a0a6591f7d739a3f596315cf7cb31e83e
Mikhail Aristov <mikhail.aristov@student.kit.edu>
2017-03-01 18:09:46 +0100

    rename sound model to signal model

---
+++ Ghul_Unity/Assets/Scripts/AI/AI_SignalModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AI_SignalModel  {
+
+	public AI_SignalModel(Data_GameState GS) {
+		recalculate(GS);	
+	}
+
+	public void recalculate(Data_GameState GS) {
+		
+	}
+
+	public double signalLikelyhood(Data_GameState GS, Data_Position receiverPos, int soundType) {
+
+
+
+		return 0;
+	}
+}
