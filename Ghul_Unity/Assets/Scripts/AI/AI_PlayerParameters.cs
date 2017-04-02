a2c136e1f15390b25a6441b3dfc25b256da47cbd
Mikhail Aristov <mikhail.aristov@student.kit.edu>
2017-04-02 16:06:07 +0200

    remove batch gradient descent, since stochastic converges faster and the data is not that big

--- Ghul_Unity/Assets/Scripts/AI/AI_PlayerParameters.cs
+++ Ghul_Unity/Assets/Scripts/AI/AI_PlayerParameters.cs
@@ -22,6 +22,7 @@ public class AI_PlayerParameters {
 	// Overall stats: How likely Toni is to run or to stand still (the sum of both should be below 1.0!)
 	public double PROB_RUNNING;
 	public double PROB_STANDING;
+
 	public double PROB_WALKING {
 		get { return (1.0 - PROB_RUNNING - PROB_STANDING); }
 		private set { return; }
@@ -52,49 +53,8 @@ public class AI_PlayerParameters {
 		Debug.LogFormat("Player movement model: {0:P3} standing, {1:P3} walking, {2:P3} running", PROB_STANDING, PROB_WALKING, PROB_RUNNING);
 	}
 
+	// Use stochastic gradient descent using the weights to update the walking weights
 	public void updateWalkingDistanceWeights(List<AI_RoomHistory> roomHistory) {
-		batchGradientDescent(roomHistory);
-		stochasticGradientDescent(roomHistory);
-	}
-
-	public double batchWeightExplore;
-	public double batchWeightItemFetch;
-	public double batchWeightDoor2Door;
-
-	public void batchGradientDescent(List<AI_RoomHistory> roomHistory) {
-		// Initialize the cumulative gradient (for batch gradient descent)
-		double cumulativeGradientExploration = 0;
-		double cumulativeGradientItemFetch = 0;
-		double cumulativeGradientDoor2Door = 0;
-		// Go through Toni's room history
-		foreach(AI_RoomHistory entry in roomHistory) {
-			// Ignore entries where the walked distance is close to zero as abnormalities
-			if(entry.cumulativeWalkedDistance < 0.1) {
-				continue;
-			}
-			//Debug.LogFormat("Room history: room {0:D} -> {1:F3} m", entry.roomId, entry.cumulativeWalkedDistance);
-			// Assuming the MSE loss function, first calculate the absolute loss
-			double loss = batchWeightExplore * entry.meanExplorationDistance
-			              + batchWeightItemFetch * entry.meanItemFetchDistance
-			              + batchWeightDoor2Door * entry.meanDoorToDoorDistance
-			              - entry.cumulativeWalkedDistance;
-			// Then, calculate and update the gradients
-			cumulativeGradientExploration += 2 * entry.meanExplorationDistance * loss;
-			cumulativeGradientItemFetch += 2 * entry.meanItemFetchDistance * loss;
-			cumulativeGradientDoor2Door += 2 * entry.meanDoorToDoorDistance * loss;
-		}
-		// Update all weights with gradient and learning rate
-		batchWeightExplore -= LEARNING_RATE_WEIGHTS * cumulativeGradientExploration / roomHistory.Count;
-		batchWeightItemFetch -= 10 * LEARNING_RATE_WEIGHTS * cumulativeGradientItemFetch / roomHistory.Count;
-		batchWeightDoor2Door -= LEARNING_RATE_WEIGHTS * cumulativeGradientDoor2Door / roomHistory.Count;
-		Debug.LogFormat("BATCH: Player room transition model: exploration weight: {0:F3}; item fetch weight: {1:F3}; door2door weight: {2:F3}", batchWeightExplore, batchWeightItemFetch, batchWeightDoor2Door);
-	}
-
-	public double stochWeightExplore;
-	public double stochWeightItemFetch;
-	public double stochWeightDoor2Door;
-
-	public void stochasticGradientDescent(List<AI_RoomHistory> roomHistory) {
 		// Randomize the roomHistory order
 		AI_Util.shuffleList<AI_RoomHistory>(roomHistory);
 		// Go through Toni's room history
@@ -105,22 +65,25 @@ public class AI_PlayerParameters {
 			}
 			//Debug.LogFormat("Room history: room {0:D} -> {1:F3} m", entry.roomId, entry.cumulativeWalkedDistance);
 			// Assuming the MSE loss function, first calculate the absolute loss
-			double loss = stochWeightExplore * entry.meanExplorationDistance
-			              + stochWeightItemFetch * entry.meanItemFetchDistance
-			              + stochWeightDoor2Door * entry.meanDoorToDoorDistance
+			double loss = WEIGHT_EXPLORATION_WALK * entry.meanExplorationDistance
+			              + WEIGHT_ITEM_FETCH_WALK * entry.meanItemFetchDistance
+			              + WEIGHT_DOOR2DOOR_WALK * entry.meanDoorToDoorDistance
 			              - entry.cumulativeWalkedDistance;
 			// Then, calculate and update the gradients
-			stochWeightExplore -= LEARNING_RATE_WEIGHTS * 2 * entry.meanExplorationDistance * loss;
-			stochWeightItemFetch -= 10 * LEARNING_RATE_WEIGHTS * 2 * entry.meanItemFetchDistance * loss;
-			stochWeightDoor2Door -= LEARNING_RATE_WEIGHTS * 2 * entry.meanDoorToDoorDistance * loss;
+			WEIGHT_EXPLORATION_WALK -= LEARNING_RATE_WEIGHTS * 2 * entry.meanExplorationDistance * loss;
+			WEIGHT_ITEM_FETCH_WALK -= 10 * LEARNING_RATE_WEIGHTS * 2 * entry.meanItemFetchDistance * loss;
+			WEIGHT_DOOR2DOOR_WALK -= LEARNING_RATE_WEIGHTS * 2 * entry.meanDoorToDoorDistance * loss;
 		}
-		Debug.LogFormat("STOCHASTIC: Player room transition model: exploration weight: {0:F3}; item fetch weight: {1:F3}; door2door weight: {2:F3}", stochWeightExplore, stochWeightItemFetch, stochWeightDoor2Door);
+		Debug.LogFormat("Player room transition model: exploration weight: {0:F3}; item fetch weight: {1:F3}; door2door weight: {2:F3}", WEIGHT_EXPLORATION_WALK, WEIGHT_ITEM_FETCH_WALK, WEIGHT_DOOR2DOOR_WALK);
+		// Perform a sanity check
+		sanityCheck();
 	}
 
 	// Performs sanity checks after each learning iteration and resets the system if necessary
 	private void sanityCheck() {
 		// Walk weights may never fall under -0.1
 		if(WEIGHT_EXPLORATION_WALK < -0.1 || WEIGHT_ITEM_FETCH_WALK < -0.1 || WEIGHT_DOOR2DOOR_WALK < -0.1) {
+			Debug.LogWarning("Abnormal walking weights detected, resetting...");
 			resetWalkingWeights();
 		}
 	}
@@ -129,12 +92,5 @@ public class AI_PlayerParameters {
 		WEIGHT_EXPLORATION_WALK = 0.1;
 		WEIGHT_ITEM_FETCH_WALK = 0.7;
 		WEIGHT_DOOR2DOOR_WALK = 0.4;
-
-		batchWeightExplore = 0.1;
-		batchWeightItemFetch = 0.7;
-		batchWeightDoor2Door = 0.4;
-		stochWeightExplore = 0.1;
-		stochWeightItemFetch = 0.7;
-		stochWeightDoor2Door = 0.4;
 	}
 }
