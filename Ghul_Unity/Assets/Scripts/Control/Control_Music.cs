1a7e98f5c6d5cffd61505fbf1ce34cbb04099d49
Mikhail Aristov <mikhail.aristov@gmail.com>
2017-06-12 18:43:32 +0200

    restore the script changes overwritten by Vivo's merge cleanup Thumbs.db again

--- Ghul_Unity/Assets/Scripts/Control/Control_Music.cs
+++ Ghul_Unity/Assets/Scripts/Control/Control_Music.cs
@@ -8,8 +8,8 @@ public class Control_Music : MonoBehaviour {
 	private Data_PlayerCharacter TONI;
 	private Data_Monster MONSTER;
 
-	private float SUICIDLE_DURATION;
-	private const float TRACK_MUTING_DURATION = 5f;
+	private const float TRACK_MUTING_DURATION = 1f;
+	private const float TRACK_UNMUTING_DURATION = 5f;
 
 	private const float MIN_PROXIMITY = 3f;
 	private const float MAX_PROXIMITY = 30f;
@@ -23,20 +23,14 @@ public class Control_Music : MonoBehaviour {
 	private bool allMuted;
 	private int currentTrackID;
 
-	public AudioSource AmbientNoise;
 	public AudioSource EncounterJingle;
+	public AudioSource ItemPlacementJingle;
 	public Control_MusicTrack[] MainTrackList;
-	public GameObject EndgameTrack;
+	public AudioSource EndgameTrack;
 
 	void Awake() {
-		SUICIDLE_DURATION = Global_Settings.read("SUICIDLE_DURATION");
 		allPaused = false;
 		allMuted = false;
-	}
-
-	void Start() {
-		AmbientNoise.mute = false;
-		AmbientNoise.volume = 0;
 	}
 
 	void Update() {
@@ -51,20 +45,22 @@ public class Control_Music : MonoBehaviour {
 		case(Control_GameState.STATE_COLLECTION_PHASE):
 			if(MONSTER.worldModel.hasMetToni) {
 				if(currentTrackID != GS.numItemsPlaced) {
-					MainTrackList[currentTrackID].muteTrack(TRACK_MUTING_DURATION);
-					MainTrackList[GS.numItemsPlaced].unmuteTrack(TRACK_MUTING_DURATION);
+					switchTracks(currentTrackID, GS.numItemsPlaced);
 					currentTrackID = GS.numItemsPlaced;
 				}
 				MainTrackList[currentTrackID].updateProximityFactor(proximityTrackVolumeFactor);
 			}
 			break;
 		case(Control_GameState.STATE_MONSTER_PHASE):
-			// By contrast, for the monster phase we place the audio source at the camera and regulate the volume
-			if(AmbientNoise.mute) {
-				AmbientNoise.mute = false;
+			if(MONSTER.worldModel.hasMetToniSinceLastMilestone) {
+				if(EndgameTrack.mute) {
+					EndgameTrack.Play();
+					EndgameTrack.mute = false;
+				}
+				if(EndgameTrack.volume < 0.999f) {
+					EndgameTrack.volume = Mathf.Lerp(EndgameTrack.volume, 1f, 0.001f);
+				}
 			}
-			float targetVolume = Mathf.Min(1.0f, TONI.timeWithoutAction / SUICIDLE_DURATION);
-			AmbientNoise.volume = Mathf.Lerp(AmbientNoise.volume, targetVolume, 0.01f);
 			break;
 		default:
 			// Ambient music should not play during other phases
@@ -78,22 +74,28 @@ public class Control_Music : MonoBehaviour {
 	void FixedUpdate() {
 		// Update the target volume of the proximity track
 		if(GS != null && !GS.SUSPENDED && GS.OVERALL_STATE == Control_GameState.STATE_COLLECTION_PHASE) {
-			switch(GS.separationBetweenTwoRooms[TONI.isIn.INDEX, MONSTER.isIn.INDEX]) {
-			case 0:
-				proximityTrackVolumeFactor = 1f;
-				break;
-			case 1:
-				proximityTrackVolumeFactor = 0.8f;
-				break;
-			case 2:
-				proximityTrackVolumeFactor = 0.6f;
-				break;
-			case 3:
-				proximityTrackVolumeFactor = 0.4f;
-				break;
-			default:
-				proximityTrackVolumeFactor = 0.2f;
-				break;
+			// Mute the proximity track as long as Toni and the monster have not met
+			if(!MONSTER.worldModel.hasMetToniSinceLastMilestone) {
+				proximityTrackVolumeFactor = 0.0001f;
+			} else {
+				// Otherwise, set it to depend on the current distance between them
+				switch(GS.separationBetweenTwoRooms[TONI.isIn.INDEX, MONSTER.isIn.INDEX]) {
+				case 0:
+					proximityTrackVolumeFactor = 1f;
+					break;
+				case 1:
+					proximityTrackVolumeFactor = 0.8f;
+					break;
+				case 2:
+					proximityTrackVolumeFactor = 0.6f;
+					break;
+				case 3:
+					proximityTrackVolumeFactor = 0.4f;
+					break;
+				default:
+					proximityTrackVolumeFactor = 0.2f;
+					break;
+				}
 			}
 		}
 	}
@@ -115,6 +117,19 @@ public class Control_Music : MonoBehaviour {
 		}
 	}
 
+	// Switches the next track to play
+	private void switchTracks(int oldTrack, int newTrack) {
+		// Mute the previous track
+		MainTrackList[oldTrack].muteTrack(TRACK_MUTING_DURATION);
+		// Play the item jingle
+		if(!ItemPlacementJingle.isPlaying) {
+			ItemPlacementJingle.Play();
+		}
+		// Unmute the next track after the jingle stops playing
+		float delay = ItemPlacementJingle.clip.length / 2f;
+		MainTrackList[newTrack].unmuteTrack(duration: TRACK_UNMUTING_DURATION, delay: delay, restart: true);
+	}
+
 	// Play the horror jingle upon the first meeting with a monster
 	public void playEncounterJingle() {
 		if(GS.OVERALL_STATE == Control_GameState.STATE_COLLECTION_PHASE) {
@@ -122,7 +137,7 @@ public class Control_Music : MonoBehaviour {
 				EncounterJingle.Play();
 			}
 			if(MainTrackList[currentTrackID].muted) {
-				MainTrackList[currentTrackID].unmuteTrack(duration: TRACK_MUTING_DURATION, delay: Global_Settings.read("ENCOUNTER_JINGLE_DURATION"), restart: true);
+				MainTrackList[currentTrackID].unmuteTrack(duration: TRACK_UNMUTING_DURATION, delay: Global_Settings.read("ENCOUNTER_JINGLE_DURATION"), restart: true);
 			}
 		}
 	}
@@ -130,34 +145,32 @@ public class Control_Music : MonoBehaviour {
 	// Pause and unpause all audio sources in the jukebox
 	private void pauseAll() {
 		if(!allPaused) {
-			AmbientNoise.Pause();
 			EncounterJingle.Pause();
 			foreach(Control_MusicTrack track in MainTrackList) {
 				track.pause();
 			}
-			// TODO Endgame track
+			EndgameTrack.Pause();
 			allPaused = true;
 		}
 	}
 
 	private void unpauseAll() {
 		if(allPaused) {
-			AmbientNoise.UnPause();
 			EncounterJingle.UnPause();
 			foreach(Control_MusicTrack track in MainTrackList) {
 				track.unpause();
 			}
-			// TODO Endgame track
+			EndgameTrack.UnPause();
 			allPaused = false;
 		}
 	}
 
 	private void muteAll() {
 		if(!allMuted) {
-			AmbientNoise.mute = true;
 			foreach(Control_MusicTrack track in MainTrackList) {
 				track.muteTrack(0);
 			}
+			EndgameTrack.mute = true;
 			allMuted = true;
 		}
 	}
