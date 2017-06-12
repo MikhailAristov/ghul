ad5c7df6d62f629fe2bb70178b2b52f8880a4976
Mikhail Aristov <mikhail.aristov@gmail.com>
2017-06-12 11:05:14 +0200

    start_integrating step sounds

---
+++ Ghul_Unity/Assets/Scripts/Control/Control_WalkingSounds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Control_WalkingSounds : MonoBehaviour {
+
+	private static List<AudioClip> walkingSounds;
+	private static List<AudioClip> runningSounds;
+
+	void Awake() {
+		
+	}
+
+	public void makeRandomWalkingStepNoise() {
+		
+	}
+
+	public void PrintEvent(string s) {
+		Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
+	}
+}
