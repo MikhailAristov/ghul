3d77c9c3c3acd46df7c71c8df08667b8fa4d9405
Mikhail Aristov <mikhail.aristov@gmail.com>
2017-06-23 10:29:33 +0200

    +blood puddles

--- Ghul_Unity/Assets/Scripts/Control/Control_CorpsePool.cs
+++ Ghul_Unity/Assets/Scripts/Control/Control_CorpsePool.cs
@@ -8,23 +8,27 @@ public class Control_CorpsePool : MonoBehaviour {
 
 	public Control_Camera MainCameraControl;
 	public GameObject HumanCorpsePrefab;
+	public GameObject BloodPuddlePrefab;
 	public GameObject HumanSkeletonPrefab;
 	public GameObject MonsterCorpsePrefab;
 
 	private List<GameObject> humanCorpses;
+	private List<GameObject> bloodPuddles;
 	private List<GameObject> monsterCorpses;
 
 	private GameObject lastHumanCorpse;
+	private GameObject lastBloodPuddle;
 	private GameObject lastMonsterCorpse;
 
 	private bool CorpsesMoved;
 
-	public const float corpseVisibilityThreshold = -0.2f;
+	public const float corpseVisibilityThreshold = -0.5f;
 
 	// Use this for initialization
 	void Awake() {
 		humanCorpses = new List<GameObject>();
 		monsterCorpses = new List<GameObject>();
+		bloodPuddles = new List<GameObject>();
 		CorpsesMoved = true;
 	}
 
@@ -37,6 +41,11 @@ public class Control_CorpsePool : MonoBehaviour {
 				if(corpse.transform.parent != this.transform && corpse != lastHumanCorpse) {
 					// Only remove the coprse if the main camera cannot see it!
 					if(!MainCameraControl.canSeeObject(corpse, corpseVisibilityThreshold)) {
+						// Replace the corpse with a drying blood puddle
+						if(corpse.name.StartsWith(HumanCorpsePrefab.name)) {
+							placeBloodPuddle(corpse.transform.parent.gameObject, corpse.transform.localPosition, corpse.GetComponentInChildren<SpriteRenderer>().flipX);
+						}
+						// Move the corpse
 						corpse.transform.parent = this.transform;
 						corpse.transform.localPosition = Vector3.zero;
 					} else {
@@ -55,6 +64,17 @@ public class Control_CorpsePool : MonoBehaviour {
 					}
 				}
 			}
+			// Ditto for blood puddles
+			foreach(GameObject puddle in bloodPuddles) {
+				if(puddle.transform.parent != this.transform && puddle != lastBloodPuddle) {
+					if(!MainCameraControl.canSeeObject(puddle, corpseVisibilityThreshold)) {
+						puddle.transform.parent = this.transform;
+						puddle.transform.localPosition = Vector3.zero;
+					} else {
+						allClear = false;
+					}
+				}
+			}
 			// Reset the CorpsesMoved flag only if no excessive corpses are visible anymore
 			CorpsesMoved = !allClear;
 		}
@@ -62,26 +82,31 @@ public class Control_CorpsePool : MonoBehaviour {
 
 	// Creates or recycles a human corpse GameObject and places it where specified
 	public GameObject placeHumanCorpse(GameObject parent, Vector2 position, bool flipped) {
-		return placeCorpse(ref humanCorpses, ref HumanCorpsePrefab, ref lastHumanCorpse, "Human Corpse #", parent, position, flipped);
+		return placeCorpse(ref humanCorpses, ref HumanCorpsePrefab, ref lastHumanCorpse, parent, position, flipped);
 	}
 
 	// Creates or recycles a human skeleton GameObject and places it where specified
 	public GameObject placeHumanSkeleton(GameObject parent, Vector2 position, bool flipped) {
-		return placeCorpse(ref humanCorpses, ref HumanSkeletonPrefab, ref lastHumanCorpse, "Human Corpse #", parent, position, flipped);
+		return placeCorpse(ref humanCorpses, ref HumanSkeletonPrefab, ref lastHumanCorpse, parent, position, flipped);
 	}
 
 	// Creates or recycles a monster corpse GameObject and places it where specified
 	public GameObject placeMonsterCorpse(GameObject parent, Vector2 position, bool flipped) {
-		return placeCorpse(ref monsterCorpses, ref MonsterCorpsePrefab, ref lastMonsterCorpse, "Monster Corpse #", parent, position, flipped);
+		return placeCorpse(ref monsterCorpses, ref MonsterCorpsePrefab, ref lastMonsterCorpse, parent, position, flipped);
+	}
+
+	// Creates or recycles a blood puddle GameObject and places it where specified
+	public GameObject placeBloodPuddle(GameObject parent, Vector2 position, bool flipped) {
+		return placeCorpse(ref bloodPuddles, ref BloodPuddlePrefab, ref lastBloodPuddle, parent, position, flipped);
 	}
 
 	// Creates or recycles any corpse GameObject and places it where specified
-	private GameObject placeCorpse(ref List<GameObject> list, ref GameObject prefab, ref GameObject lastCorpse, string name, GameObject parent, Vector2 position, bool flipped) {
+	private GameObject placeCorpse(ref List<GameObject> list, ref GameObject prefab, ref GameObject lastCorpse, GameObject parent, Vector2 position, bool flipped) {
 		GameObject result = null;
 		// First check if there are any free corpses in the pool
 		foreach(GameObject corpse in list) {
 			// corpse.name == prefab.name ensures that a regular corpse is not placed instead of a skeleton in the opening cutscene upon restarting the game
-			if(corpse.transform.parent == this.transform && corpse.name == prefab.name) {
+			if(corpse.transform.parent == this.transform && corpse.name.StartsWith(prefab.name)) {
 				result = corpse;
 				break;
 			}
@@ -90,7 +115,7 @@ public class Control_CorpsePool : MonoBehaviour {
 		if(result == null) {
 			result = Instantiate(prefab) as GameObject;
 			list.Add(result);
-			result.name = name + list.Count.ToString();
+			result.name = prefab.name + " #" + list.Count.ToString();
 		}
 		// Move the corpse
 		result.transform.parent = parent.transform;
@@ -110,6 +135,10 @@ public class Control_CorpsePool : MonoBehaviour {
 		foreach(GameObject corpse in monsterCorpses) {
 			corpse.transform.parent = this.transform;
 			corpse.transform.localPosition = Vector3.zero;
+		}
+		foreach(GameObject puddle in bloodPuddles) {
+			puddle.transform.parent = this.transform;
+			puddle.transform.localPosition = Vector3.zero;
 		}
 		CorpsesMoved = false;
 	}
