458e5f7aa32564f2c7af07d1622443ebde69e796
Mikhail Aristov <mikhail.aristov@gmail.com>
2017-07-08 21:41:23 +0200

    bugfix

--- Assets/Scripts/Control/Control_GameState.cs
+++ Assets/Scripts/Control/Control_GameState.cs
@@ -648,6 +648,7 @@ public class Control_GameState : MonoBehaviour {
 		if(GS == null) {
 			resetGameState();
 			setAdditionalParameters(newGame: true);
+			StartCoroutine(playOpeningCutscene());
 		}
 		GS.SUSPENDED = false;
 	}
@@ -658,7 +659,6 @@ public class Control_GameState : MonoBehaviour {
 		if(GS != null) {
 			Control_Persistence.saveToDisk(GS);
 		}
-		Screen.SetResolution(640, 480, false);
 		Application.Quit();
 		Debug.Log("Quitting game...");
 	}
